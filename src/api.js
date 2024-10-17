47dfe7dadc37de7a2d904ea927f3eeca425a5806
ash97531 <ashwani.97531@gmail.com>
2024-10-17 23:35:06 +0530

    changed api url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://basys-backend-h18c.onrender.com/api';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
